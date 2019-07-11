15057ce7d2d3043ab5ea87f16b43b485248ccb29
mzfr <Mehtab.zafar98@gmail.com>
2019-07-11 09:57:52 +0530

    Fix CSS path

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,8 @@ from flask import jsonify, Flask
 app = Flask(__name__)
 from app import views
 
+app.static_folder = 'static'
+app.config['STATIC_FOLDER'] = 'static'
+
 app.run(host="127.0.0.1", port=7000)
 
